a82b4dba77f8b74db73e5205ff55acfa9387e1e5
daniel souza <workdndsza@gmail.com>
2022-07-31 21:15:02 -0300

    lendo todos os carros do banco

--- src/Routes/ApiRoute.js
+++ src/Routes/ApiRoute.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { Carro } = require('../Models')
 
+router.get('/', async (req, res) => {
+  const carros = await Carro.findAll()
+  return res.json(carros)
+})
+
 router.post('/', async (req, res) => {
   const data = req.body
   console.log(data)
